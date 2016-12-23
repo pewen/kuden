71149581d75a4f8d78cd1c6c925d9b05ebd690e1
fnbellomo <fnbellomo@gmail.com>
2016-12-23 13:42:37 -0300

    change server to openshift

--- _site/js/config.js
+++ _site/js/config.js
@@ -2,5 +2,7 @@
    General game confing
 */
 
-// Server url base
+// Server UNRC url base
 var url_base = "https://mail.evelia.unrc.edu.ar/kuden";
+// Server OpenShift url base
+var url_base = "https://kuden-pewen.rhcloud.com";

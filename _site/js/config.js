b38c364fadce4afc43d0ad2cf6bd84365720cfc3
dbellomo <dbellomo@pewen.eco.unrc.edu.ar>
2017-05-11 10:58:13 -0300

    arreglo ortografía

--- _site/js/config.js
+++ _site/js/config.js
@@ -3,8 +3,9 @@
 */
 
 // Server UNRC url base
-var url_base = "https://mail.evelia.unrc.edu.ar/kuden";
+var url_base = "http://200.117.225.66";
+// var url_base = "https://mail.evelia.unrc.edu.ar/kuden";
 // Server OpenShift url base
-var url_base = "https://kuden-pewen.rhcloud.com";
+// var url_base = "https://kuden-pewen.rhcloud.com";
 // localhost (test)
 // var url_base = "http://127.0.0.1:5000";

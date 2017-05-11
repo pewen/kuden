7579628568ba69488947ebacba55288405e51806
fnbellomo <fnbellomo@gmail.com>
2017-05-11 11:16:33 -0300

    change server url to heroku

--- js/config.js
+++ js/config.js
@@ -3,9 +3,16 @@
 */
 
 // Server UNRC url base
-var url_base = "http://200.117.225.66";
 // var url_base = "https://mail.evelia.unrc.edu.ar/kuden";
+
 // Server OpenShift url base
 // var url_base = "https://kuden-pewen.rhcloud.com";
+
+// RPI
+// var url_base = "http://200.117.225.66";
+
+// Heroku
+var url_base = "https://frozen-gorge-23665.herokuapp.com";
+
 // localhost (test)
 // var url_base = "http://127.0.0.1:5000";

c77b2ce0f51f2e9ca47dbff09566abab0ddf7ac1
Daniel Bellomo <dbellomo@gmail.com>
2017-05-10 22:04:27 -0300

    change server address

--- js/config.js
+++ js/config.js
@@ -6,6 +6,6 @@
 var url_base = "http://200.117.225.66";
 // var url_base = "https://mail.evelia.unrc.edu.ar/kuden";
 // Server OpenShift url base
-var url_base = "https://kuden-pewen.rhcloud.com";
+// var url_base = "https://kuden-pewen.rhcloud.com";
 // localhost (test)
 // var url_base = "http://127.0.0.1:5000";
